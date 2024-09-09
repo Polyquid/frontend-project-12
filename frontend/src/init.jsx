1be9ff220434cf4ec8bc01a4d962a838c9e71ad9
Rakhimzhan <zhuba.sz@gmail.com>
2024-09-09 11:33:00 +0600

    Rollbar  init

--- frontend/src/init.jsx
+++ frontend/src/init.jsx
@@ -1,13 +1,13 @@
 import { Provider as RollbarProvider, ErrorBoundary } from '@rollbar/react';
 import Rollbar from 'rollbar';
-import i18next from 'i18next';
-import { I18nextProvider, initReactI18next } from 'react-i18next';
-import { Provider } from 'react-redux';
-import { RouterProvider } from 'react-router-dom';
-import resources from './locales/index.js';
-import router from './utils/router.jsx';
-import store from './services/index.js';
-import App from './components/App.jsx';
+// import i18next from 'i18next';
+// import { I18nextProvider, initReactI18next } from 'react-i18next';
+// import { Provider } from 'react-redux';
+// import { RouterProvider } from 'react-router-dom';
+// import resources from './locales/index.js';
+// import router from './utils/router.jsx';
+// import store from './services/index.js';
+// import App from './components/App.jsx';
 
 const TestError = () => {
   const a = null;
@@ -15,17 +15,17 @@ const TestError = () => {
 };
 
 const init = async () => {
-  const i18nextInstance = i18next.createInstance();
-  await i18nextInstance
-    .use(initReactI18next)
-    .init({
-      resources,
-      lng: 'ru',
-      interpolation: {
-        escapeValue: false,
-      },
-      debug: true,
-    });
+  // const i18nextInstance = i18next.createInstance();
+  // await i18nextInstance
+  //   .use(initReactI18next)
+  //   .init({
+  //     resources,
+  //     lng: 'ru',
+  //     interpolation: {
+  //       escapeValue: false,
+  //     },
+  //     debug: true,
+  //   });
 
   const rollbarConfig = {
     accessToken: '765d232201e348538ba66a7b8b67c4ea',
@@ -36,13 +36,13 @@ const init = async () => {
     <RollbarProvider config={rollbar}>
       <ErrorBoundary>
         <TestError />
-        <I18nextProvider i18n={i18nextInstance}>
+        {/* <I18nextProvider i18n={i18nextInstance}>
           <Provider store={store}>
             <RouterProvider router={router}>
               <App />
             </RouterProvider>
           </Provider>
-        </I18nextProvider>
+        </I18nextProvider> */}
       </ErrorBoundary>
     </RollbarProvider>
   );
