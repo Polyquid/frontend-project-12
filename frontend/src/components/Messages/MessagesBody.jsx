74d2b1f735027981ea627ac44800d211fd664b79
Rakhimzhan <zhuba.sz@gmail.com>
2024-09-12 12:43:05 +0600

    refactor logic of MessagesBody

--- frontend/src/components/Messages/MessagesBody.jsx
+++ frontend/src/components/Messages/MessagesBody.jsx
@@ -3,29 +3,22 @@ import { useEffect, useRef } from 'react';
 const MessagesBody = ({ currentMessages }) => {
   const messagesEndRef = useRef(null);
 
-  const scrollToBottom = () => {
-    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
-  };
-  const renderMessages = () => {
-    if (!currentMessages) {
-      return null;
-    }
-    return currentMessages.map(({ body, username, id }) => (
-      <div key={id} className="text-break mb-2">
-        <b>{username}</b>
-        {': '}
-        {body}
-      </div>
-    ));
-  };
-
   useEffect(() => {
+    const scrollToBottom = () => {
+      messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+    };
     scrollToBottom();
   }, [currentMessages]);
 
   return (
     <div className="chat-messages overflow-auto px-5 ">
-      {renderMessages()}
+      {currentMessages?.map(({ body, username, id }) => (
+        <div key={id} className="text-break mb-2">
+          <b>{username}</b>
+          {': '}
+          {body}
+        </div>
+      ))}
       <div ref={messagesEndRef} />
     </div>
   );
